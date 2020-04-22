3e484d50b341584c766c0b0dd3ceafb5b88f96df
Darkflame72 <31436575+Darkflame72@users.noreply.github.com>
2020-04-22 21:06:36 +1200

    added all block names

--- blocks.js
+++ blocks.js
@@ -50,6 +50,154 @@ function onModLoad() {
     cyan_glazed_terracotta = newBlock("cyan_glazed_terracotta", "PiNQE8g");
     cyan_terracotta = newBlock("cyan_terracotta", "PiNQE8g");
     cyan_wool = newBlock("cyan_wool", "WatiJLr");
+    dark_oak_log = newBlock("dark_oak_log", ["Fnq4Knx", "Fnq4Knx", "Nn72YqR"]);
+    dark_oak_planks = newBlock("dark_oak_planks", "djLr10k");
+    dark_prismarine = newBlock("dark_prismarine", "dieMkUL");
+    dead_brain_coral_block = newBlock("dead_brain_coral_block", "RCITBcf");
+    dead_buble_coral_block = newBlock("dead_buble_coral_block", "URbfAiI");
+    dead_fire_coral_block = newBlock("dead_fire_coral_block", "diaxwPr");
+    dead_horn_coral_block = newBlock("dead_horn_coral_block", "5gMJHQz");
+    dead_tube_coral_block = newBlock("dead_tube_coral_block", "03FQ30q");
+    diamond_block = newBlock("diamond_block", "WatiJLr");
+    diamond_ore = newBlock("diamond_ore", "WatiJLr");
+    diorite = newBlock("diorite", "WatiJLr");
+    
+    // TODO
+
+
+    // emerald_ore
+    // emeralnd_block
+    // end_stone
+    // end_stone_bricks
+    // dead_fire_coral_blockfletching_table
+    // blast_furnaceglowstone
+    // gold_block
+    // gold_ore
+    // granite 
+    // grass_block_snow
+    // grass_path
+    // gray_concrete
+    // gray_concrete_powder
+    // gray_glazed_terracotta
+    // gray_terracotta 
+    // gray_wool
+    // green_concrete_powder
+    // green_concrete 
+    // green_glazed_terracotta
+    // green_terracotta 
+    // green_wool 
+    // hay_bale
+    // honey_block
+    // honeycomb_block
+    // horn_coral_block 
+    // ice 
+    // iron_block 
+    // jack_o_lantern
+    // jukebox
+    // jungle_log
+    // jungle_planks
+    // lapis_block
+    // lapis_ore
+    // light_blue_concrete
+    // light_blue_concrete_powder
+    // light_blue_glazed_terracotta
+    // light_blue_terracotta 
+    // light_blue_wool
+    // light_gray_concrete
+    // light_gray_concrete_powder
+    // light_gray_glazed_terracotta
+    // light_gray_terracotta 
+    // light_gray_wool
+    // lime_concrete
+    // lime_concrete_powder
+    // lime_glazed_terracotta
+    // lime_terracotta 
+    // lime_wool
+    // loom
+    // magenta_concrete
+    // magenta_concrete_powder
+    // magenta_glazed_terracotta
+    // magenta_terracotta 
+    // magenta_wool
+    // melon 
+    // mossy_cobblestone
+    // mossy_stone_bricks
+    // mushroom_stem
+    // mycellium
+    // nether_bricks
+    // nether_quartz_ore
+    // nether_wart_block
+    // netherrack 
+    // note_block
+    // oak_log 
+    // oak_planks 
+    // observer // nightmare
+    // obsidian
+    // orange_concrete
+    // orange_concrete_powder
+    // orange_glazed_terracotta
+    // orange_terracotta 
+    // orange_wool
+    // packed_ice
+    // pink_concrete
+    // pink_concrete_powder
+    // pink_glazed_terracotta
+    // pink_terracotta 
+    // pink_wool
+    // podzol
+    // polished_andesite
+    // polished_diorite
+    // polished_granite
+    // prismarine_bricks
+    // pumpkin
+    // purple_concrete
+    // purple_concrete_powder
+    // purple_glazed_terracotta
+    // purple_terracotta 
+    // purple_wool
+    // quartz_block
+    // quartz_pillar
+    // red_concrete
+    // red_concrete_powder
+    // red_glazed_terracotta
+    // red_terracotta 
+    // red_wool
+    // red_nether_bricks
+    // red_sand 
+    // red_sandstone 
+    // redstone_block
+    // redstone_lamp
+    // sandstone 
+    // scaffolding
+    // slime_block
+    // smithing_table
+    // smoker
+    // smooth_stone
+    // soul_sand
+    // sponge 
+    // spruce_log
+    // spruce_planks
+    // stone_bricks
+    // stripped_acacia_log
+    // stripped_birch_log
+    // stripped_dark_oak_log
+    // stripped_jungle_log
+    // stripped_dark_oak_log
+    // stripped_spruce_log
+    // terracotta 
+    // tnt 
+    // tube_coral_block 
+    // wet_sponge 
+    // white_concrete
+    // white_concrete_powder
+    // white_glazed_terracotta
+    // white_terracotta 
+    // white_wool
+    // yellow_concrete
+    // yellow_concrete_powder
+    // yellow_glazed_terracotta
+    // yellow_terracotta 
+    // yellow_wool
 
     // Thanks to Name With Exactly 32 Characters!#4004 for this.
     newCommand("cmd",
